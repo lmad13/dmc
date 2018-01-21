62978fc540b229a9d180ac6867eddc2e752bbd87
Lindsey R. Madison <lindsey@Lindseys-MBP.Home>
2018-01-21 14:56:05 -0800

    implemented descendant weighting

--- MatrixElementsDMC.py
+++ MatrixElementsDMC.py
@@ -3,16 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 au2wn=219474.63
 nWalkers=2000
-Wfn0=dmc.wavefunction(nWalkers,'harmonic',plotting=False)
+
+Wfn0=dmc.wavefunction(nWalkers,'half harmonic',plotting=False)
 v_ref=[]
 pop_0=[]
 nReps=5
-nSteps=100
+nSteps=1000
 
 v_ref_array_0=np.zeros((nReps,nSteps))
 pop_array_0=np.zeros((nReps,nSteps))
-nBins=50
+nBins=30
 xhists_0=np.zeros((nReps,nBins))
+
+Wfn0.setX(Wfn0.xcoords+.001)
 for n in range(nReps):
     vref,pop,x,d=Wfn0.propagate(Wfn0.xcoords,nSteps)
     vref=np.array(vref)
@@ -20,7 +23,8 @@ for n in range(nReps):
     pop_0.append(np.average(pop[nSteps/2:]))
     v_ref_array_0[n,:]=vref*au2wn
     pop_array_0[n,:]=pop
-    xHist,bin_edges=np.histogram(x, bins=nBins, range=(-1.0,1.0),density=True)
+    vrd,popd,xd,descendantWeights=Wfn0.propagate(x,25)
+    xHist,bin_edges=np.histogram(x, bins=nBins, range=(-1.0,1.0),density=True,weights=descendantWeights)
     xhists_0[n]=xHist
 
 print 'average energy',np.average(v_ref)
@@ -31,20 +35,22 @@ np.savetxt('vrefForNOTFixed.data', v_ref_array_0.T)
 np.savetxt('popForNOTFixed.data', pop_array_0.T)
 print Wfn0.omega/2.0
 
-Wfn1=dmc.wavefunction(nWalkers,'harmonic',plotting=False)
+Wfn1=dmc.wavefunction(nWalkers,'half harmonic',plotting=False)
+Wfn1.setX(Wfn1.xcoords+.1)
 v_ref_1=[]
 pop_1=[]
 v_ref_array_1=np.zeros((nReps,nSteps))
 pop_array_1=np.zeros((nReps,nSteps))
 xhists_1=np.zeros((nReps,nBins))
 for n in range(nReps):
-    vref,pop,x,d=Wfn1.propagate(Wfn1.xcoords,nSteps,setV_ref=True,ConstantV_ref=1000.0/au2wn)
+    vref,pop,x,d=Wfn1.propagate(Wfn1.xcoords,nSteps,setV_ref=True,ConstantV_ref=3000.0/au2wn)
     vref=np.array(vref)
     v_ref_1.append(np.average(vref[nSteps/2:])*au2wn)
     v_ref_array_1[n,:]=vref*au2wn
     pop_array_1[n,:]=pop
     pop_1.append(np.average(pop[nSteps/2:]))
-    xHist,bin_edges=np.histogram(x, bins=nBins, range=(-1.0,1.0),density=True)
+    vrd,popd,xd,descendantWeights=Wfn1.propagate(x,25,setV_ref=True,ConstantV_ref=3000.0/au2wn)
+    xHist,bin_edges=np.histogram(x, bins=nBins, range=(-1.0,1.0),density=True,weights=descendantWeights)
     xhists_1[n]=xHist
 print 'v1 average energy',np.average(v_ref_1)
 print 'v1 standard deviation', np.std(v_ref_1)
@@ -72,20 +78,6 @@ for n in range(nReps):
     plt.plot(bin_center,xhists_0[n],c='r')
     plt.plot(bin_center,xhists_1[n],c='b')
 plt.show()
-#wavefunction class
-
-#initalize wavefunction
-#place walkers on HO
-
-#function propagate
-#propagate wavefunction for nsteps WITHOUT kmnowing the final v_ref
-#take in walker positions, V, nsteps, delta t
-#return vref measurment (avearged over 1000 steps, only at equilibrium), snapshots of wavefunction every 500 t steps after equilibration
-
-#function CalcDesc
-#calculate descendant weights
-#take in walker positions, V, nsteps, delta t         
-#return the number of descendants
 
 
 
